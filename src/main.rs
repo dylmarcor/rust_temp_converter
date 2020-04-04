9ea9a320afb00163498c36cbd488962aec5b8c61
DylMarCor <dylmarcor@protonmail.com>
2020-04-04 11:13:50 -0700

    add faherheit and celsius functionality

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,68 @@ fn main() {
     println!("Temperatue Converter\n\n\n");
 
     loop {
-        println!("Enter the temperature you want to convert\n");
-        println!("Enter \"END\" to halt the program");
+        println!("Would you like Celsius or Fahrenheit?");
 
         let mut choice = String::new();
-        let mut temperature = String::new();
 
         io::stdin()
-            .read_line(&mut temperature)
+            .read_line(&mut choice)
             .expect("Failed to read line");
 
-        if temperature.trim()  == "END" {
-            break;
-        }
+        if choice.trim() == "C" {
 
-        let temperature: f64 = match temperature.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue
-        };
+            println!("Enter the tempc you want to convert\n");
+            println!("Enter \"END\" to halt the program");
 
-        println!("You inputted: {}", temperature);
+            let mut tempc = String::new();
 
-        println!("{}", (tempc(temperature)).to_string());
+            io::stdin()
+                .read_line(&mut tempc)
+                .expect("Failed to read line");
+
+            if tempc.trim()  == "END" {
+                break;
+            }
+
+            let tempc: f64 = match tempc.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue
+            };
+
+            println!("You inputted: {}", tempc);
+
+            println!("{}", (calc_tempc(tempc)).to_string());
+
+        } else if choice.trim() == "F" {
+
+            println!("Enter the tempc you want to convert\n");
+            println!("Enter \"END\" to halt the program");
+
+            let mut tempf = String::new();
+
+            io::stdin()
+                .read_line(&mut tempf)
+                .expect("Failed to read line");
+
+            if tempf.trim()  == "END" {
+                break;
+            }
+
+            let tempf: f64 = match tempf.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue
+            };
+
+            println!("You inputted: {}", tempf);
+
+            println!("{}", (calc_tempf(tempf)).to_string());
     }
 }
 
-fn tempc(temp: f64) -> f64 {
+fn calc_tempc(temp: f64) -> f64 {
    temp / 5.0 * 9.0 + 32.0
 }
 
-fn tempf(temp: f64) -> {
+fn calc_tempf(temp: f64) -> {
     (temp - 32.0) * 5.0 / 9.0
 }
