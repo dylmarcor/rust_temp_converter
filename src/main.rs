91976f9e18a24ed3cea5ecb6bbb9e7000c33b86d
DylMarCor <dylmarcor@protonmail.com>
2020-04-04 10:35:15 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main() {
+    println!("Temperatue Converter");
+
+    loop {
+        println!("Enter the temperature you want to convert");
+        println!("Enter \"END\" to halt the program");
+
+        let mut temperature = String::new();
+
+        io::stdin()
+            .read_line(&mut temperature)
+            .expect("Failed to read line");
+
+        if temperature.trim()  == "END" {
+            break;
+        }
+
+        let temperature: f64 = match temperature.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("You inputted: {}", temperature);
+
+        println!("{}", (tempc(temperature)).to_string());
+    }
+}
+
+fn tempc(temp: f64) -> f64 {
+   temp / 5.0 * 9.0 + 32.0
+}
